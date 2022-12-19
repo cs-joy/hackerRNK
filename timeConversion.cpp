62ed96079f1d5cae0b38e1a5b491718e4558170f
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-19 19:35:18 +0600

    Create timeConversion.cpp

---
+++ timeConversion.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+string timeConversion(string s)
+{
+  int n = s.length();
+  
+  string hour_string = s.substr(0, 2);
+  
+  int hour = stoi(hour_string);
+  
+  string newTime;
+  
+  if(s[n-2] == 'A')
+  {
+    if(hour == 12)
+    {
+      new_hour = "00";
+      newTime = new_hour.append(s.substr(2, 6);
+      return newTime;
+    }
+    else
+    {
+      return s.substr(0, n-2);
+    }
+  }
+  
+}
+
+int main()
+{
+  return 0;
+}

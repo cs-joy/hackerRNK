a14ca778035f582c18dc13b8f51bc2cdbc507747
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-19 10:06:13 +0600

    minMax sum

---
+++ minMaxSum.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+void minMaxSum(vector<int> arr)
+{
+  int n = arr.size();
+  
+  int sum = 0;
+  
+  ll max = arr[0], min = arr[0];
+  
+  for(int i=0; i<n; i++)
+  {
+    sum += arr[i];
+  }
+  
+  for(int j=1; j<n; j++)
+  {
+    if(arr[j] > max)
+      max = arr[j];
+    if(arr[j] < min)
+      min = arr[j];
+  }
+  
+  cout << sum - max << " " << sum - min;
+}
+
+int main()
+{
+  vector<int> arr {1, 2, 3, 4, 5};
+  minMaxSum(arr);
+  
+  return 0;
+}

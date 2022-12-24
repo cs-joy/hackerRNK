ba6615b8b4fbbb5d6a205f6fcac35628e10c3a74
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-25 03:27:54 +0600

    Update pointer.cpp

--- pointer.cpp
+++ pointer.cpp
@@ -1,12 +1,19 @@
 // ref: https://www.hackerrank.com/challenges/c-tutorial-pointer/problem?isFullScreen=true
 
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main()
+void update(int* a, int* b)
 {
-    
+    int num = *a;
+    *a += *b;
+    *b = abs(num - *b);
+}
+
+
+void introductionOfPointer()
+{
     int val=5;
     int* ptr;
     
@@ -22,6 +29,17 @@ int main()
     cout << "address of var : " << &val << endl;
     cout << "address of ptr : " << ptr << endl;
      cout << "value of ptr : " << *ptr << endl;
+}
+
+int main()
+{
+    int n1, n2;
+    int *pn1 = &n1;
+    int *pn2 = &n2;
+    
+    cin >> n1 >> n2;
+    update(pn1, pn2);
+    cout << n1 << " " <<n2;
     
     return 0;
 }

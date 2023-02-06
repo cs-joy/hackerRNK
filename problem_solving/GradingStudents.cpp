9cd066d1564a54d85db268e56965cbe57533473b
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-02-06 21:20:57 +0600

    solved!

--- problem_solving/GradingStudents.cpp
+++ problem_solving/GradingStudents.cpp
@@ -1,51 +1,77 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-vector<int> gradingStudents(vector<int> grades)
-{
-    int limit = 0;
-    while(limit<100)
-    {
-        limit += 5;
-        for (int i=0; i<grades.length; i++)
-        {
-            if (grades[i] < limit)
-            {
-                //
+string ltrim(const string &);
+string rtrim(const string &);
+
+vector<int> gradingStudents(vector<int> grades) {
+    for (int i=0; i<grades.size(); i++) {
+        if (grades[i] >= 38) {
+            int rem = grades[i] % 5;
+            if (rem >= 3) {
+                grades[i] += 5 - rem;
             }
         }
     }
-
     return grades;
-}
-
-void display(vector<int> grades)
-{
-    for (int i=0; i<grades.size(); i++)
-    {
-        cout << grades[i] << " ";
-    }
 }
 
 int main()
 {
-    //
-    int n, gpoint;
-    cin >> n;
+    ofstream fout(getenv("OUTPUT_PATH"));
 
-    vector<int> grades;
+    string grades_count_temp;
+    getline(cin, grades_count_temp);
 
-    while(n>0)
-    {
-        n--;
-        cin >> gpoint;
+    int grades_count = stoi(ltrim(rtrim(grades_count_temp)));
 
-        grades.push_back(gpoint);
+    vector<int> grades(grades_count);
+
+    for (int i = 0; i < grades_count; i++) {
+        string grades_item_temp;
+        getline(cin, grades_item_temp);
+
+        int grades_item = stoi(ltrim(rtrim(grades_item_temp)));
+
+        grades[i] = grades_item;
     }
 
-    display(grades);
+    vector<int> result = gradingStudents(grades);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
 
     return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
 }
